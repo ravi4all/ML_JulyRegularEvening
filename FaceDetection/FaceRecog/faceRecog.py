ff2e5f6c205f3a4b6290a95dd2b445e225718033
ravi4all <ravikanttyagi.2295@gmail.com>
2018-07-25 16:13:35 +0530

    Add files via upload

--- FaceDetection/FaceRecog/faceRecog.py
+++ FaceDetection/FaceRecog/faceRecog.py
@@ -14,7 +14,7 @@ users = {
     1 : 'Raman'
 }
 
-labels = np.zeros([40,1])
+labels = np.zeros((40,1))
 labels[:20,:] = 0.0
 labels[20:,:] = 1.0
 
@@ -26,38 +26,52 @@ data = np.concatenate([face_1, face_2])
 def distance(x1,x2):
     return np.sqrt(((x2 - x1) ** 2).sum())
 
+##def knn(train,x, k=5):
+##    m = train.shape[0]
+##    dist = []
+##
+##    for i in range(m):
+##        dist.append([train[i], x])
+##
+##    dist = np.asarray(dist)
+##    index = np.argsort(dist)
+##    sorted_labels = labels[index][:k]
+##    counts = np.unique(sorted_labels, return_counts=True)
+##    return counts[0][np.argmax(counts[1])]
+
 def knn(train,x, k=5):
     m = train.shape[0]
     dist = []
-
-    for i in range(m):
-        dist.append([train[i], x])
-
+    for ix in range(m):
+        # compute distance from each point and store in dist
+        dist.append(distance(x, train[ix]))
     dist = np.asarray(dist)
-    index = np.argsort(dist)
-    sorted_labels = labels[index][:k]
+    indx = np.argsort(dist)
+    print("Index...",indx)
+    sorted_labels = labels[indx][:k]
+    print("Sorted...",sorted_labels)
     counts = np.unique(sorted_labels, return_counts=True)
+    print("Count...",counts)
     return counts[0][np.argmax(counts[1])]
 
 while True:
-    ret, img = capture.read()
-    if ret:
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        face = dataset.detectMultiScale(gray, 1.3)
-        for x, y, w, h in face:
-            faceComponent = img[y:y+h, x:x+w, :]
-            faceComp = cv2.resize(faceComponent, (50,50))
-            cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 4)
-
-            label = knn(data,faceComp.flatten())
-            username = users.get(int(label))
-            cv2.putText(img, username,(x,y),font,1,(255,0,0),2)
-
-        cv2.imshow('result', img)
-        if cv2.waitKey(1) == 27:
+    ret, frame = capture.read()
+    if ret == True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = dataset.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            face_component = frame[y:y + h, x:x + w, :]
+            fc = cv2.resize(face_component, (50, 50))
+            lab = knn(data,fc.flatten())
+            text = users[int(lab)]
+            cv2.putText(frame, text, (x, y), font, 1, (255, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.imshow('face', frame)
+        k = cv2.waitKey(33) & 0xFF
+        if k == 27:
             break
     else:
-        print("Some Error")
+        print('Error')
 
-cv2.destroyAllWindows()
 capture.release()
+cv2.destroyAllWindows()
